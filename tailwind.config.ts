45037c08bca048b96ae31084f695ba35a5139ca5
xsalles <xs.salles@gmail.com>
2025-06-25 23:20:28 -0300

    config: added the correct settings to use tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
+
