452e2a1869b5825e9cb2b179fa9a2ca309c6250c
xsalles <xs.salles@gmail.com>
2025-06-27 11:00:04 -0300

    feat: created component header

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60 bg-black flex items-center justify-between px-8 py-4">
+      <Image
+        src={"/logo.svg"}
+        alt="Logo da site.set com cor branca em um fundo preto"
+        width={115}
+        height={32}
+      />
+
+      <nav className="flex items-center gap-6">
+        <Link className="text-white" href={"/inicio"}>Início</Link>
+        <Link className="text-white" href={"/blog"}>Blog</Link>
+        <Button variant={'secondary'} className="rounded-[999px]">Começar</Button>
+      </nav>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Button } from "@/components/ui/button";
+import Header from "@/components/Header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,10 +16,7 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-4 bg-black">
-      <h2 className="text-4xl text-white">Titleeads</h2>
-    <Button variant={"secondary"}>
-      Pedro
-    </Button>
+      <Header />
     </div>
   );
 } 
