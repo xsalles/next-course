a8629132c8b3ece3010bf476f98a737ae2d5bbc7
xsalles <xs.salles@gmail.com>
2025-06-28 23:34:52 -0300

    feat: created the component header with a logical that changes the color of text depending on the route

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,8 +2,15 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
+
+  const isHomePage = router.pathname === "/";
+  const isBlogPage = router.pathname.startsWith("/blog");
+
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60 bg-black flex items-center justify-between px-8 py-4">
       <Image
@@ -14,9 +21,27 @@ export default function Header() {
       />
 
       <nav className="flex items-center gap-6">
-        <Link className="text-white" href={"/inicio"}>Início</Link>
-        <Link className="text-white" href={"/blog"}>Blog</Link>
-        <Button variant={'secondary'} className="rounded-[999px]">Começar</Button>
+        <Link
+          className={cn(
+            "font-medium transition-colors ease-in-out duration-300 hover:text-blue-400",
+            isHomePage ? "text-blue-600" : "text-white"
+          )}
+          href={"/"}
+        >
+          Início
+        </Link>
+        <Link
+          className={cn(
+            "font-medium transition-colors ease-in-out duration-300 hover:text-blue-400",
+            isBlogPage ? "text-blue-600" : "text-white"
+          )}
+          href={"/blog"}
+        >
+          Blog
+        </Link>
+        <Button variant={"secondary"} className="rounded-[999px]">
+          Começar
+        </Button>
       </nav>
     </header>
   );
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header/header";
+
+export default function Blog() {
+  return (
+    <>
+      <Header />
+      <h1>blog</h1>
+    </>
+  );
+}
