afa8855a99c0e60c28cc24bf1a0383122751f2f7
xsalles <xs.salles@gmail.com>
2025-07-01 11:46:08 -0300

    feat: created the hero section

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
+
+export default function HeroSection() {
+  return (
+    <section className="flex mt-20 items-center gap-14 justify-around">
+      <div className="flex flex-col items-start text-left gap-14">
+        <div className="flex flex-col gap-8">
+          <h1 className="text-white text-heading-hg max-w-[640px]">
+            Venda seus produtos como afiliado em um único lugar
+          </h1>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-3">
+              <Image
+                src="/clock.svg"
+                alt="Pequeno ícone de um relógio"
+                width={20}
+                height={20}
+              />
+              <p className="text-body-md text-white">
+                Crie o seu site em menos de 5 minutos
+              </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <Image
+                src="/store.svg"
+                alt="Pequeno ícone de uma loja"
+                width={20}
+                height={20}
+              />
+              <p className="text-body-md text-white">
+                Acompanhe e otimize seu negócio online
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start gap-4">
+          <Button className="w-full rounded-full">Criar loja grátis</Button>
+          <p className="text-gray-300 text-body-xs">
+            Não precisa de cartão de crédito
+          </p>
+        </div>
+      </div>
+      <Image
+        alt="Ilustração de imagem"
+        src={"/illustration.png"}
+        width={471}
+        height={491}
+      />
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import Header from "@/components/Header/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-black text-white">
-      <h1>Initial Page</h1>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 }
