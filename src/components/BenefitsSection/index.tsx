4c6dc35ac2740640d09adae649da6498afd02f18
xsalles <xs.salles@gmail.com>
2025-07-03 11:10:23 -0300

    feat: added all background from figma.

--- src/components/BenefitsSection/index.tsx
+++ src/components/BenefitsSection/index.tsx
@@ -2,12 +2,15 @@ import { HeartHandshake, PaintbrushVertical, Store } from "lucide-react";
 
 export default function BenefitsSection() {
   return (
-    <section className="w-full bg-gray-500  flex flex-col items-center justify-center gap-8 p-14">
-      <h3 className="text-heading-xl text-white  text-center">
+    <section className="w-full bg-gray-500  flex flex-col items-center justify-center gap-8 p-10 relative">
+
+      <div className="absolute z-0 inset-0 hidden md:block bg-[url('/backgroundBenefits.png')] bg-cover bg-center bg-no-repeat" />
+
+      <h3 className="text-heading-xl text-white  text-center z-10">
         Sua loja de afiliados, simples, do jeito que deveria ser
       </h3>
-      <div className=" flex flex-col gap-4 w-full md:w-[68%] items-center justify-center md:flex-row">
-        <article className="flex text-left gap-4 flex-col text-white bg-blue-400 p-6 rounded-lg w-full md:w-[33.3%]">
+      <div className="z-10 flex flex-col gap-4 w-full lg:w-[68%] items-center justify-center lg:flex-row">
+        <article className="lg:h-[195px] flex text-left gap-4 flex-col text-white bg-blue-400 p-6 rounded-lg w-full lg:w-[33.3%]">
           <PaintbrushVertical className="w-9 h-9 bg-blue-300 p-2 rounded-lg" />
           <div className="flex flex-col gap-2">
             <h5 className="text-heading-sm">Personalize seu site</h5>
@@ -17,7 +20,7 @@ export default function BenefitsSection() {
             </p>
           </div>
         </article>
-        <article className="flex text-left gap-4 flex-col text-white bg-cyan-300 p-6 rounded-lg w-full md:w-[33.3%]">
+        <article className="lg:h-[195px] flex text-left gap-4 flex-col text-white bg-cyan-300 p-6 rounded-lg w-full lg:w-[33.3%]">
           <Store className="w-9 h-9 bg-cyan-200 p-2 rounded-lg" />
           <div className="flex flex-col gap-2">
             <h5 className="text-heading-sm">Venda de qualquer loja</h5>
@@ -27,7 +30,7 @@ export default function BenefitsSection() {
             </p>
           </div>
         </article>
-        <article className="flex text-left gap-4 flex-col text-white bg-blue-400 p-6 rounded-lg w-full md:w-[33.3%]">
+        <article className="lg:h-[195px] flex text-left gap-4 flex-col text-white bg-blue-400 p-6 rounded-lg w-full lg:w-[33.3%]">
           <HeartHandshake className="w-9 h-9 bg-blue-300 p-2 rounded-lg" />
           <div className="flex flex-col gap-2">
             <h5 className="text-heading-sm">Receba suporte amigável</h5>
--- src/components/CtaSection/index.tsx
+++ src/components/CtaSection/index.tsx
@@ -5,11 +5,13 @@ import { Button } from "../ui/button";
 export default function CtaSection() {
   return (
     <section className="w-full flex items-center flex-col gap-14 bg-gray-400 relative pb-16">
-      <Store className="w-16 h-16 p-4 rounded-full bg-cyan-300 text-cyan-100 translate-y-[-30px]" />
-      <h3 className="text-white text-heading-xl md:w-[450px] text-center">
+     <div className="z-0 absolute inset-0 hidden md:block bg-[url('/backgroundCTA.png')] bg-cover bg-center bg-no-repeat" />
+
+      <Store className="z-10 w-16 h-16 p-4 rounded-full bg-cyan-300 text-cyan-100 translate-y-[-30px]" />
+      <h3 className="text-white text-heading-xl md:w-[450px] text-center z-10">
         Crie uma loja online e inicie suas vendas ainda hoje
       </h3>
-      <Button className="rounded-full">
+      <Button className="rounded-full z-10">
         Criar loja grátis
         <ArrowRight />
       </Button>
