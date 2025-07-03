7233910f652b9fa5cfa190c11c4188de000fc6de
xsalles <xs.salles@gmail.com>
2025-07-03 12:12:09 -0300

    feat: created the first post using md with contentlayer.

---
+++ contentlayer.config.ts
@@ -0,0 +1,20 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.md`,
+  fields: {
+    title: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    image: { type: 'string', required: true },
+  },
+  computedFields: {
+    slug: {
+        type: 'string',
+        resolve: (post) => post._raw.flattenedPath.replace('.md', '')
+    }
+  },
+}))
+
+export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] })
