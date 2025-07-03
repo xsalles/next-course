956dd008e09cff0d55ae3a9f574020aa8573e01c
xsalles <xs.salles@gmail.com>
2025-07-03 14:19:47 -0300

    feat: moved all code from blog page to templates/blog/index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,20 +1,5 @@
-import Search from "@/components/Search";
+import BlogList from "@/templates/blog";
 
 export default function Blog() {
-  return (
-    <div className="h-screen items-center flex flex-col mt-20 bg-black text-white px-48 py-20">
-      <div className="flex flex-col md:flex-row md:items-end gap-6 item md:justify-between w-full">
-        <div className="flex flex-col items-start gap-3">
-          <p className="bg-cyan-300 text-cyan-100 px-3 py-2 text-body-xs font-medium rounded-sm w-fit">
-            BLOG
-          </p>
-          <h3 className="text-heading-xl w-[440px]">
-            Dicas e estratégias para impulsionar seu negócio
-          </h3>
-        </div>
-        
-        <Search />
-      </div>
-    </div>
-  );
+  return <BlogList />
 }
---
+++ src/templates/blog/index.tsx
@@ -0,0 +1,20 @@
+import Search from "@/components/Search";
+
+export default function BlogList() {
+  return (
+    <div className="h-screen items-center flex flex-col mt-20 bg-black text-white px-48 py-20">
+      <div className="flex flex-col md:flex-row md:items-end gap-6 item md:justify-between w-full">
+        <div className="flex flex-col items-start gap-3">
+          <p className="bg-cyan-300 text-cyan-100 px-3 py-2 text-body-xs font-medium rounded-sm w-fit">
+            BLOG
+          </p>
+          <h3 className="text-heading-xl w-[440px]">
+            Dicas e estratégias para impulsionar seu negócio
+          </h3>
+        </div>
+
+        <Search />
+      </div>
+    </div>
+  );
+}
