f10d24769f9339365b8e02b8534723e63f0761d5
xsalles <xs.salles@gmail.com>
2025-07-03 12:48:58 -0300

    feat: added animation into the search component.

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,8 +2,8 @@ import { SearchIcon } from "lucide-react";
 
 export default function Search() {
   return (
-    <form className="flex items-center bg-transparent border border-gray-400 rounded-lg px-4 gap-3 h-10">
-      <SearchIcon className="w-4 h-4  text-gray-300" />
+    <form className="flex items-center bg-transparent border border-gray-400 rounded-lg px-4 gap-3 h-10 transition-colors duration-200 ease-in-out focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300">
+      <SearchIcon className="w-4 h-4 text-gray-300" />
       <input
         type="text"
         placeholder="Buscar"
