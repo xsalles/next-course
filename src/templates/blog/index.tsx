da52858ca4c46a338e036b7dd12f5e327b498795
xsalles <xs.salles@gmail.com>
2025-07-07 20:48:43 -0300

    feat: now i can search and the content will be displayed dynamically.

--- src/templates/blog/index.tsx
+++ src/templates/blog/index.tsx
@@ -1,45 +1,44 @@
 import Search from "@/components/Search";
-import { allPosts } from '.contentlayer/generated'
-import { compareDesc } from 'date-fns'
+import { allPosts } from ".contentlayer/generated";
+import { compareDesc } from "date-fns";
 import PostCard from "@/components/PostCard";
 import { CtaSection } from "../landing-page/sections";
 import { useRouter } from "next/router";
 
 export default function BlogList() {
-  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
-  const router = useRouter()
+  const router = useRouter();
   const contentSearched = router.query.post as string;
 
-  const pageTitle = contentSearched ? `Resultados para "${contentSearched}"` : 'Dicas e estratégias para impulsionar seu negócio'
+  const pageTitle = contentSearched
+    ? `Resultados para "${contentSearched}"`
+    : "Dicas e estratégias para impulsionar seu negócio";
+
+  const posts = contentSearched
+    ? allPosts.filter((post) => 
+        post.title.toLowerCase()?.includes(contentSearched.toLowerCase())
+      )
+    : allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
 
   return (
     <>
-    
-    
-    <div className="items-center flex flex-col mt-20 bg-black text-white px-4 md:px-48 pt-20 mb-32">
-      <div className="flex flex-col md:flex-row md:items-end gap-6 item md:justify-between w-full">
-        <div className="flex flex-col items-start gap-3">
-          <p className="bg-cyan-300 text-cyan-100 px-3 py-2 text-body-xs font-medium rounded-sm w-fit">
-            BLOG
-          </p>
-          <h3 className="text-heading-xl max-w-[440px]">
-            {pageTitle}
-          </h3>
+      <div className="items-center flex flex-col mt-20 bg-black text-white px-4 md:px-48 pt-20 mb-32">
+        <div className="flex flex-col md:flex-row md:items-end gap-6 item md:justify-between w-full">
+          <div className="flex flex-col items-start gap-3">
+            <p className="bg-cyan-300 text-cyan-100 px-3 py-2 text-body-xs font-medium rounded-sm w-fit">
+              BLOG
+            </p>
+            <h3 className="text-heading-xl max-w-[440px]">{pageTitle}</h3>
+          </div>
+
+          <Search />
         </div>
-
-        <Search />
+        <div className="grid grid-cols-1  md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 w-full ">
+          {posts.map((post, key) => (
+            <PostCard {...post} key={key} />
+          ))}
+        </div>
       </div>
-     <div className="grid grid-cols-1  md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 w-full ">
-      {
-        posts.map((post, key) => (
-          <PostCard {...post} key={key}/>
-        ))
-      }
-     </div>
-
-
-    </div>
-    <CtaSection />
+      <CtaSection />
     </>
   );
 }
